597282115ddf58207d1c951b98c020faf3d339df
gaithern <gaithern@gmail.com>
2026-02-26 18:16:15 -0600

    Update kh1_lua_library.lua

--- scripts/io_packages/kh1_lua_library.lua
+++ scripts/io_packages/kh1_lua_library.lua
@@ -612,7 +612,7 @@ function is_pressed(button_array, only)
     local input_bits = merge_tables(merge_tables(merge_tables(ReadBits(inputAddress), ReadBits(inputAddress+1)), ReadBits(inputAddress+2)), ReadBits(inputAddress+3))
     local bitmap = {"Select", "L3", "R3", "Start", "DPad U", "DPad R", "DPad D", "DPad L",
               "L2", "R2", "L1", "R1", "Triangle", "Circle", "X", "Square",
-              nil, nil, nil, nil, "Right Analog Stick U", "Right Analog Stick R", "Right Analog Stick D", "Right Analog Stick L"
+              nil, nil, nil, nil, "Right Analog Stick U", "Right Analog Stick R", "Right Analog Stick D", "Right Analog Stick L",
               nil, nil, nil, nil, "Left Analog Stick U", "Left Analog Stick R", "Left Analog Stick D", "Left Analog Stick L"}
     expected_bitmap = {}
     for k,v in pairs(bitmap) do
